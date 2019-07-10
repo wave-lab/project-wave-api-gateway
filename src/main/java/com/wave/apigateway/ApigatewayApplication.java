b873de66a996b0eb22e21d8505e817a091be24d9
yeonghyeonSeok <syh4834@naver.com>
2019-07-11 03:55:03 +0900

    라우팅 수정

--- src/main/java/com/wave/apigateway/ApigatewayApplication.java
+++ src/main/java/com/wave/apigateway/ApigatewayApplication.java
@@ -1,7 +1,14 @@
 package com.wave.apigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+
+import javax.activation.DataSource;
 
 @SpringBootApplication
 public class ApigatewayApplication {
